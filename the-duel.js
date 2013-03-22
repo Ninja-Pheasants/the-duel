b4eca62efaf88900641301de8e541ef7d559f4f4
NKjoep <nkjoep@gmail.com>
2013-03-22 18:20:21 +0100

    upd

--- the-duel.js
+++ the-duel.js
@@ -20,24 +20,33 @@ app.configure(function() {
 	app.set("view options", {layout: false});
 	app.engine('html', require('ejs').renderFile);
 	app.use(express.cookieParser()); 
+	app.use(express.cookieParser('this is a very long hash. Please forget it! ok? :E'));
+	app.use(express.session({
+		key: 'Ninja Pheasants',
+		secret: 'this is a very long hash. Please forge it! ok? :E'
+	}));
 });
 app.engine('html', require('ejs').renderFile);
 
-
+var LABEL_DUELLO_CHIUSO = 'Il duello è chiuso.';
 var DUEL_IS_OPEN = false;
+var FILE_VOTI = "VOTI-data.txt";
 
+//provo a leggere dal file dei voti per boostrappare i dati
 VOTI =  {
 	aperto: DUEL_IS_OPEN,
 	giuria: { a: 0, b: 0 },
 	pubblico: { a: 0, b: 0 },
 };
-
-var LABEL_DUELLO_CHIUSO = 'Il duello è chiuso.';
+try {
+	VOTI = JSON.parse(fs.readFileSync(FILE_VOTI));
+}
+catch (e) {console.log("File dei voti", FILE_VOTI, "non trovato");}
 
 //using
 app.use(function(req, res, next){ //on each request coming to our server...
 	VOTI.aperto = DUEL_IS_OPEN;
-	fs.writeFile('VOTI-data.txt', JSON.stringify(VOTI), function (err) {
+	fs.writeFile(FILE_VOTI, JSON.stringify(VOTI), function (err) {
 		if (err) throw err;
 	});
 	next(); //and move on...
@@ -53,34 +62,26 @@ app.get('/', function(req, res) {
 		VOTI: VOTI
 	});
 });
+
 app.get('/vota1', function(req, res) {
-	var messaggio = LABEL_DUELLO_CHIUSO;
-	if (DUEL_IS_OPEN) {
-		VOTI.pubblico.a = VOTI.pubblico.a +1; 
-		messaggio = 'Hai votato 1';
-	}
 	res.render('index', { 
 		_ : underscore,
-		messaggio: messaggio,
+		messaggio: vota('a'),
 		risultati: null,
 		DUEL_IS_OPEN: DUEL_IS_OPEN,
 		VOTI: VOTI
 	});
 });
 app.get('/vota2', function(req, res) {
-	var messaggio = LABEL_DUELLO_CHIUSO;
-	if (DUEL_IS_OPEN) {
-		VOTI.pubblico.b = VOTI.pubblico.b +1;
-		messaggio = 'Hai votato 2';
-	}
 	res.render('index', { 
 		_ : underscore,
-		messaggio: messaggio,
+		messaggio: vota('b'),
 		risultati: null,
 		DUEL_IS_OPEN: DUEL_IS_OPEN,
 		VOTI: VOTI
 	});
 });
+
 app.get('/risultati', function(req, res) {
 	res.send(JSON.stringify(VOTI));
 });
@@ -89,10 +90,26 @@ app.get('/open', function(req, res) {
 	DUEL_IS_OPEN = true;
 	res.redirect("/");
 })
+
 app.get('/close', function(req, res) {
 	DUEL_IS_OPEN = false;
 	res.redirect("/");
 })
+
+
+//utility
+function vota(candidato, giuria) {
+	if (!DUEL_IS_OPEN) return LABEL_DUELLO_CHIUSO;
+	var messaggio = 'Hai votato: ';
+	if(giuria==true && candidato) {
+		VOTI.giuria[candidato] = VOTI.giuria[candidato] +1;
+	}
+	else {
+		VOTI.pubblico[candidato] = VOTI.pubblico[candidato] +1;
+	}
+	return messaggio + candidato;
+} 
+
 
 //start
 app.listen(options.port);
